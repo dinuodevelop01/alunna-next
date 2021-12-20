e539a6ba448abf726eae7571ff012854cef2afb4
Kato Kenta <developdinuo01@gmail.com>
2021-12-20 15:19:26 +0800

    updated

---
+++ pages/_document.js
@@ -0,0 +1,36 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
+
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    {/* <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+                    <link rel="icon" type="image/png" href={require("../images/favicon.png")}></link> */}
+
+                    <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
+                    {/* <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" /> */}
+                    <meta name="theme-color" content="#000000" />
+                    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+                    <link href="https://fonts.googleapis.com/css?family=Inter:300,400,500,700" rel="stylesheet" />
+                    <meta name="description"
+                        content="Alunna" />
+                    <meta name="keywords" content="alunna" />
+                    <meta name="author" content="Alunna" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument;
